f4871c2f922825a7b3e51ebf1a9dccc577d8f382
Sebastiabmvm <sebastian.vargas13@inacapmail.cl>
2025-01-26 18:39:03 -0300

    Actualización de archivos HTML, se agrega página de habilidades + carrusel, y se agrega la primera página guía para subir los proyectos

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
+
+
+
+let indiceActual = 0;
+
+function moverCarrusel(direccion) {
+    const carrusel = document.querySelector('.carrusel');
+    const items = document.querySelectorAll('.carrusel-item');
+    const totalItems = items.length;
+
+    // Calcula el nuevo índice
+    indiceActual = (indiceActual + direccion + totalItems) % totalItems;
+
+    // Mueve el carrusel
+    carrusel.style.transform = `translateX(-${indiceActual * 100}%)`;
+}
+
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const hash = window.location.hash;
+  if (hash) {
+      const targetElement = document.querySelector(hash);
+      if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+  }
+});
